e2679c4d14bb251c0a38ec319d64f2cacc8d854a
malinda <malisur490@gmail.com>
2024-06-16 23:41:47 +0530

    create table backend complete

--- web/controllers/orders.js
+++ web/controllers/orders.js
@@ -1,53 +1,61 @@
-const {getOrderDetailsById,CreateOrder} = require("../services/orders")
+const {
+  getOrderDetailsById,
+  OrderTable,
+  RecieverTable,
+  SenderTable,
+  GetsenderID,
+  GetRecieverID,
+  SenderTele,
+  RecieverTele,
+} = require("../services/orders");
 
-module.exports ={
-    getOrderDetailsById: (req ,res) =>{
-        const id = req.params.orderId;
-        getOrderDetailsById(id,(error,results)=>{
-            if(error){
-                res.json({
-                    success:0,
-                    message:error
-                })
-            }
-            if(results.length==0){
-                res.json({
-                    success:101,
-                    message: "invalid order id"
-                })
-            }
-            else if(results){
-                res.json({
-                    success: 200,
-                    message:results
-                })
-            }
+module.exports = {
+  getOrderDetailsById: (req, res) => {
+    const id = req.params.orderId;
+    getOrderDetailsById(id, (error, results) => {
+      if (error) {
+        res.json({
+          success: 0,
+          message: error,
+        });
+      }
+      if (results.length == 0) {
+        res.json({
+          success: 101,
+          message: "invalid order id",
+        });
+      } else if (results) {
+        res.json({
+          success: 200,
+          message: results,
+        });
+      }
+    });
+  },
+
+  CreateOrder: async (req, res) => {
+    const data = req.body;
+    console.log(data)
+    try {
+        await SenderTable(data);
+        await RecieverTable(data);
+        const senderID = await GetsenderID();
+        const recieverID = await GetRecieverID();
+        console.log(senderID,recieverID)
+        await SenderTele(senderID,data.stelephone)
+        await RecieverTele(recieverID,data.rtelephone)
+        await OrderTable(data,recieverID,senderID)
+        
+        return res.json({
+            success:1,
+            message: "order placed successfully",
         })
-    },
-    CreateOrder: (req ,res) =>{
-        const data = req.body
-        console.log(data)
-        return
-        // CreateOrder(data,(error,results)=>{
-        //     if(error){
-        //         res.json({
-        //             success:0,
-        //             message:error
-        //         })
-        //     }
-        //     if(results.length==0){
-        //         res.json({
-        //             success:101,
-        //             message: "invalid order id"
-        //         })
-        //     }
-        //     else if(results){
-        //         res.json({
-        //             success: 200,
-        //             message:results
-        //         })
-        //     }
-        // })
-    }
 
-}
+    } catch (error) {
+      return res.json({
+        success: 0,
+        message: error,
+      });
+    }
+  },
+};
