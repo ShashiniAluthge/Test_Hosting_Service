5a3d9c912216408d75a3aeedd8917ab33f978509
ShashiniAluthge <156067964+ShashiniAluthge@users.noreply.github.com>
2024-07-08 22:47:18 +0530

    Update index.js

--- index.js
+++ index.js
@@ -10,18 +10,18 @@ app.use(cookieParser())
 const userRouter = require("./mobile/routes/users");  //  api/mobile/users/login
 
 const orderRouter = require("./mobile/routes/orders.js");
-// const webOrderRouter = require("./web/routes/orders.js");
-// const webBranchuser = require("./web/routes/branchuser.js");
+const webOrderRouter = require("./web/routes/orders.js");
+const webBranchuser = require("./web/routes/branchuser.js");
 
-// const webAdmin = require("./web/routes/admin.js");
+const webAdmin = require("./web/routes/admin.js");
 
 app.use(express.json());
 app.use("/api/mobile/users", userRouter);
 app.use("/api/mobile/orders", orderRouter);
 
-// app.use("/orders", webOrderRouter);
-// app.use("/admin", webAdmin);
-// app.use("/branchuser",webBranchuser);
+app.use("/orders", webOrderRouter);
+app.use("/admin", webAdmin);
+app.use("/branchuser",webBranchuser);
 
 const PORT = process.env.PORT || 9000;
 app.listen(PORT, () => {
